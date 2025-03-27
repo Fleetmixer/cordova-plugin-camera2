65228a225c14c02eada0570a46a290ea5468f696
Malte Jesgarzewsky <malte@jesgarzews.ky>
2025-03-27 15:50:30 +0100

    Update CDVCamera.m

--- src/ios/CDVCamera.m
+++ src/ios/CDVCamera.m
@@ -190,7 +190,7 @@ static NSString* toBase64(NSData* data) {
                             [weakSelf sendNoPermissionResult:command.callbackId];
                         }]];
                         [alertController addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Settings", nil) style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+                            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] options:@{} completionHandler:nil];
                             [weakSelf sendNoPermissionResult:command.callbackId];
                         }]];
                         [weakSelf.viewController presentViewController:alertController animated:YES completion:nil];
